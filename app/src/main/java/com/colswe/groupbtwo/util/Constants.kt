e1ccd3a22d9e8f2aa34c2629c3e6c42078215551
Sergio Vargas <colombia00028@gmail.com>
2026-01-01 14:26:19 -0500

    last push
    Thanks for all

--- app/src/main/java/com/colswe/groupbtwo/util/Constants.kt
+++ app/src/main/java/com/colswe/groupbtwo/util/Constants.kt
@@ -3,7 +3,7 @@ package com.colswe.groupbtwo.util
 object Constants {
 
 
-    const val BASE_URL_ROUTES = "https://kimiko-bannered-mordantly.ngrok-free.dev"
+    const val BASE_URL_ROUTES = "mobile.runpath.duckdns.org"
 
 
 }
